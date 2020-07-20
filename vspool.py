ff1834e62e59b38b7f3ca5f850c95d4b6529fdb2
Bill Ola Rasmussen <billra@gmail.com>
2020-07-20 12:39:04 +0200

    vspool initial py file

---
+++ vspool.py
@@ -0,0 +1,39 @@
+# V plotter with spool kinematics
+# Bill Ola Rasmussen
+# version 0.1
+from math import sqrt, acos, atan2, sin, cos, pi
+
+def vspoolinverse(Cx,Cy,Px,Py,r):
+	h = sqrt((Px - Cx) ** 2 + (Py - Cy) ** 2)
+	b = acos(r / h)
+	m = atan2(Py - Cy,Px - Cx)
+	Tx = Cx + r * cos(m + b)
+	Ty = Cy + r * sin(m + b)
+	print('circle intersect point:', Tx,Ty)
+
+	n = atan2(Ty - Cy, Tx - Cx)
+	a = pi / 2 - n
+	c = a * r
+	print('circular section length:', c)
+	s = sqrt((Px - Tx) ** 2 + (Py - Ty) ** 2)
+	cs = c + s
+	return cs
+
+def main():
+	print('V Spool Kinematics')
+
+	Cx,Cy = 0,4 # center of spool
+	Px,Py = 7,-1 # point being plotted
+	r = 3 # radius of spool
+
+	cs = vspoolinverse(Cx,Cy,Px,Py,r)
+	print('control line length:', cs)
+
+	Py = 0
+	cs = vspoolinverse(Cx,Cy,Px,Py,r)
+	print('control line length:', cs)
+
+	print('done.')
+
+if __name__ == "__main__":
+	main()
