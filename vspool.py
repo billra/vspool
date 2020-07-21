d664bdce71bec44e7607e7275fa183f27d92722b
Bill Ola Rasmussen <billra@gmail.com>
2020-07-21 07:46:33 +0200

    calculation for spool wound in opposite direction

--- vspool.py
+++ vspool.py
@@ -4,14 +4,34 @@
 from math import sqrt, acos, atan2, sin, cos, pi
 
 def vspoolinverse(Cx,Cy,Px,Py,r):
+	# right triangle calculation to find tangent point
 	h = sqrt((Px - Cx) ** 2 + (Py - Cy) ** 2)
 	b = acos(r / h)
 	m = atan2(Py - Cy,Px - Cx)
 	Tx = Cx + r * cos(m + b)
 	Ty = Cy + r * sin(m + b)
+	# find length of arc
 	n = atan2(Ty - Cy, Tx - Cx)
 	a = pi / 2 - n
 	c = a * r
+	# find length of straight line
+	s = sqrt((Px - Tx) ** 2 + (Py - Ty) ** 2)
+	# return total length
+	cs = c + s
+	return cs
+
+def vspoolinversecc(Cx,Cy,Px,Py,r): # counter clockwise
+	# right triangle calculation to find tangent point
+	h = sqrt((Px - Cx) ** 2 + (Py - Cy) ** 2)
+	b = acos(r / h)
+	m = atan2(Py - Cy,Px - Cx)
+	Tx = Cx + r * cos(m - b) # note
+	Ty = Cy + r * sin(m - b) # note
+	# find length of arc
+	n = atan2(Ty - Cy, Tx - Cx)
+	a = pi*1.5 + n # note
+	c = a * r
+	# find length of straight line
 	s = sqrt((Px - Tx) ** 2 + (Py - Ty) ** 2)
 	cs = c + s
 	return cs
